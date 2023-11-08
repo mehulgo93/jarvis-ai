20488ad7d3a7d8260e62e2f36f36d6d877a4ab55
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-11-08 17:44:45 -0500

    some new capabilities

--- main.py
+++ main.py
@@ -6,6 +6,45 @@ import os
 import webbrowser
 from datetime import datetime
 import openai
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+OPENAI_API_KEY="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"
+PORCUPINE_ACCESS_KEY="5EcgrAgHCFO7qawq3s5PSNBRhD7Ow6SVAofKDXaZiCJBwdcMWU2gjw=="
+
+# Spotify credentials (replace with your actual credentials)
+SPOTIFY_CLIENT_ID = "38a89f9ba6474f51bca14ba918e8daf4"
+SPOTIFY_CLIENT_SECRET = "5dbff4466b974b3c9d12210919c13220"
+SPOTIFY_REDIRECT_URI = "http://localhost:8888/callback"  # Example redirect URI
+
+# Initialize the Spotipy client with the necessary OAuth scope for playback control
+scope = 'user-modify-playback-state,user-read-playback-state,playlist-read-private'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                                               client_secret=SPOTIFY_CLIENT_SECRET,
+                                               redirect_uri=SPOTIFY_REDIRECT_URI,
+                                               scope=scope))
+
+def play_spotify_track(track_name, artist_name=None):
+    # Search for the track with or without the artist's name
+    query = f'track:{track_name}'
+    if artist_name:
+        query += f' artist:{artist_name}'
+    results = sp.search(query, type='track', limit=1)
+    tracks = results['tracks']['items']
+
+    if tracks:
+        track_uri = tracks[0]['uri']
+        sp.start_playback(uris=[track_uri])  # Assuming a Spotify device is active
+        say(f"Playing {track_name} by {artist_name if artist_name else 'unknown artist'}.")
+    else:
+        say("I couldn't find the track you requested.")
+
+def get_spotify_recommendations(genre=None):
+    results = sp.recommendations(seed_genres=[genre] if genre else None, limit=5)
+    tracks = results['tracks']
+    say("Here are some recommendations:")
+    for track in tracks:
+        say(f"{track['name']} by {track['artists'][0]['name']}")
 
 def say(text):
     os.system(f"say '{text}'")
@@ -33,19 +72,44 @@ def take_command(recognizer, microphone):
         except Exception as e:
             print("I didn't catch that. What did you say?")
             return "None"
+        
+# Global variable to maintain conversation history
+conversation_history = []
+
 def get_openai_response(query):
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
+    # Add the latest query to the conversation history
+    conversation_history.append(f"User: {query}\n")
+
+    # Construct the prompt with the conversation history
+    conversation_prompt = "".join(conversation_history[-5:])  # Use the last 5 exchanges
+    conversation_prompt += "Jarvis:"
+
     try:
         response = openai.Completion.create(
-            engine="text-davinci-003",  # Or whichever model you prefer
-            prompt=query,
-            max_tokens=150
+            engine="text-davinci-003",
+            prompt=conversation_prompt,
+            max_tokens=150,
+            temperature=0.7,  # Adjust this to change the randomness of the responses
+            stop=["User:", "Jarvis:"],  # Stop sequences for conversation
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6  # Adjust this to make the assistant more 'assertive'
         )
-        return response.choices[0].text.strip()
+        
+        # Extract the response text and update the conversation history
+        response_text = response.choices[0].text.strip()
+        conversation_history.append(f"Jarvis: {response_text}\n")
+
+        return response_text
     except openai.error.OpenAIError as e:
         print(f"OpenAI API error: {e}")
         return "I'm sorry, I encountered an error while processing your request."
+
+def process_general_knowledge(query):
+    response = get_openai_response(query)
+    say(response)
 
 def handle_command(query):
     sites = [
@@ -77,10 +141,8 @@ def handle_command(query):
         now = datetime.now()
         say(f"Sir, the time is {now.strftime('%I:%M %p')}")
         return
-    elif "what is" in query.lower() or "who is" in query.lower() or "tell me about" in query.lower():
-        response = get_openai_response(query)
-        say(response)
-        return
+    else:
+        process_general_knowledge(query)
 
     # Add more commands here if needed
 
