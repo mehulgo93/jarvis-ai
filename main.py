5eb1e02868dac3c2f3b62dbb8510547a93d02d43
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-11-07 20:16:06 -0500

    some changes

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 import speech_recognition as sr
 import os
+import webbrowser
 
 def say(text):
     os.system(f"say {text}")
 
 def take_command():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Ready to listen...")
-        r.pause_threshold = 1
+     r = sr.Recognizer()
+     with sr.Microphone() as source:
+        # r.pause_threshold =  0.6
         audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some Error Occurred. Sorry from Jarvis"
 
-    try:
-        query = r.recognize_google(audio, language="en-in")
-        print(f"User said: {query}\n")
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-        return "None"
-    except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
-        return "None"
-    return query
 
 if __name__ == '__main__':
     print('Jarvis A.I. is starting...')
-    say("Hello I am Jarvis A.I")
+    say("Jarvis A.I")
     while True:
         print("Listening...")
-        text = take_command()
-        if text != "None":
-            say(text)
+        query = take_command()
+        sites = [["google", "https://www.google.com"],
+    ["youtube", "https://www.youtube.com"],
+    ["facebook", "https://www.facebook.com"],
+    ["baidu", "https://www.baidu.com"],
+    ["wikipedia", "https://www.wikipedia.org"],
+    ["amazon", "https://www.amazon.com"],
+    ["X", "https://www.x.com"],
+    ["instagram", "https://www.instagram.com"],
+    ["linkedin", "https://www.linkedin.com"],
+    ["netflix", "https://www.netflix.com"],]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
