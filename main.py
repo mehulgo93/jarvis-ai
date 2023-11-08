79ccad1a921dac87b5b1c45610f74b3f5ffc88ff
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-11-07 20:59:54 -0500

    adding openai capability

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import speech_recognition as sr
 import os
 import webbrowser
+import openai
+import datetime
 
 def say(text):
     os.system(f"say {text}")
 
+def greet_according_to_time():
+    current_hour = datetime.now().hour
+
+    if 5 <= current_hour < 12:
+        say("Good morning Sir")
+    elif 12 <= current_hour < 17:
+        say("Good afternoon Sir")
+    elif 17 <= current_hour < 21:
+        say("Good evening Sir")
+    else:
+        say("Good night Sir")
+
 def take_command():
      r = sr.Recognizer()
      with sr.Microphone() as source:
-        # r.pause_threshold =  0.6
+        r.pause_threshold =  0.6
         audio = r.listen(source)
         try:
             print("Recognizing...")
@@ -21,7 +35,7 @@ def take_command():
 
 if __name__ == '__main__':
     print('Jarvis A.I. is starting...')
-    say("Jarvis A.I")
+    say(greet_according_to_time)
     while True:
         print("Listening...")
         query = take_command()
@@ -39,3 +53,22 @@ if __name__ == '__main__':
             if f"Open {site[0]}".lower() in query.lower():
                 say(f"Opening {site[0]} sir...")
                 webbrowser.open(site[1])
+        if "play music" in query:
+            spotify_liked_songs_uri = "spotify:playlist:37i9dQZF1DXcBWIGoYBM5M"
+            os.system(f"open {spotify_liked_songs_uri}")
+        elif "the time" in query:
+            hour = datetime.datetime.now().strftime("%H")
+            min = datetime.datetime.now().strftime("%M")
+            say(f"Sir time is {hour} and {min} minutes")
+        elif "Using artificial intelligence".lower() in query.lower():
+            ai(prompt=query)
+        elif "open facetime".lower() in query.lower():
+            os.system(f"open /System/Applications/FaceTime.app")
+        elif "Jarvis Quit".lower() in query.lower():
+            exit()
+        elif "reset chat".lower() in query.lower():
+            chatStr = ""
+        else:
+            print("Chatting...")
+            chat(query)
+
